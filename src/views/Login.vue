d08bc0277c1feaa769ca1b64a37cd376d2763b29
ascenciodev <alessandroascencio@protonmail.com>
2021-01-11 12:26:37 -0300

    login router

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="column is-centered notification is-info">
+        <h1 class="title">Login de Usuário</h1>
+        <hr>
+            <div v-if="error != undefined" class="notification is-danger">
+                        {{error}}
+            </div>
+
+            <div class="columns is-centered is-4">
+                <div class="column is-half">
+                    <p>E-mail</p>
+                    <input type="email" placeholder="E-mail" class="input" v-model="email">
+                    <p>Senha</p>
+                    <input type="password" placeholder="Senha" class="input" v-model="password">
+                    <hr>
+                    <button type="submit" class="button is-success" @click="login">Entrar</button>
+                </div>            
+            </div>
+    </div>
+</template>
+
+<script>
+//import axios from 'axios'
+export default {
+    data() {
+        return {
+            password: '',
+            email: '',
+            error: undefined
+        }
+    },
+    methods: {
+        login() {
+            console.log('login')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
