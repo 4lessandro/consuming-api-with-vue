3fe6cfc67be51000adaa9991f2da39318ac76872
ascenciodev <alessandroascencio@protonmail.com>
2021-01-11 13:01:45 -0300

    router guard lst

---
+++ src/views/Users.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="column is-centered notification is-info">
+        <h1 class="title">Painel Administrativo</h1>
+        <hr>
+
+            <div class="columns is-centered is-4">
+                <div class="column is-half">
+
+                </div>            
+            </div>
+    </div>
+</template>
+
+<script>
+//import axios from 'axios'
+
+</script>
+
+<style scoped>
+
+</style>
